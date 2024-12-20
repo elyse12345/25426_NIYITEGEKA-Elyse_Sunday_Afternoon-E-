8304a061c72a0a9ea117cf9de16c4c8a90b9ed6a
elyse12345 <niyitegekaelisee28@gmail.com>
2024-12-20 17:46:37 +0200

    Add files via upload

---
+++ assignment oop/ArithmeticExceptionExample.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class ArithmeticExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // Division by zero
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic exception: " + e.getMessage());
+        }
+    }
+}
---
+++ assignment oop/ClassCastExceptionEx.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class ClassCastExceptionEx {
+    public static void main(String[] args) {
+        try {
+            Object obj = "String";
+            Integer num = (Integer) obj; // Invalid type cast
+        } catch (ClassCastException e) {
+            System.out.println("Class cast exception: " + e.getMessage());
+        }
+    }
+}
